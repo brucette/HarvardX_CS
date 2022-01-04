5c56596b3780c88bfe42ef8b94a9459e5b432767
brucette <89015988+brucette@users.noreply.github.com>
2022-01-04 14:21:00 +0000

    Tue, Jan 4, 2022, 2:21 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,19 +4,21 @@
 int main(void)
 {
     int height;
+    //request height from user
     do
     {
         height = get_int("Enter height of pyramid: ");
     }
     while (height < 1 || height > 8);
 
+    //
     for (int i = 1; i <= height; i++)
     {
-        for (int j = 1; j <= height; )
+        for (int j = 1; j <= height; j++)
         {
             printf("#");
         }
         printf("\n");
-        j++
     }
+    printf("\n");
 }
