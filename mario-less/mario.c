d9171b10e5a0dbe03aa0ec471c825cef7f73dd8a
brucette <89015988+brucette@users.noreply.github.com>
2022-01-04 15:17:17 +0000

    Tue, Jan 4, 2022, 3:17 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -14,7 +14,7 @@ int main(void)
     //
     for (int i = 1; i <= height; i++)
     {
-        for (int j = 1; j <= height; j++)
+        for (int j = 1; j <= i; j++)
         {
             printf("#");
         }
