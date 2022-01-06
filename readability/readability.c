a46bd9f1a5434169df1d0079e12aac3c3abce459
brucette <89015988+brucette@users.noreply.github.com>
2022-01-06 17:10:19 +0000

    Thu, Jan 6, 2022, 5:10 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -19,7 +19,7 @@ int main(void)
     float per_hundred = 100 / count_words(text);
     int L = (count_letters(text) / count_words(text)) * per_hundred;    //average number of letters per 100 words
     int S = (count_sentences(text) / count_words(text)) * per_hundred;  //average number of sentences per 100 words
-    int index = 0.0588 * L - 0.296 * S - 15.8
+    int index = 0.0588 * L - 0.296 * S - 15.8;
 
 }
 
