de27b90e3270466391ff131365a1ecfe73a48738
brucette <89015988+brucette@users.noreply.github.com>
2022-01-07 17:55:07 +0000

    Fri, Jan 7, 2022, 5:55 PM +00:00

--- caesar/test.c
+++ caesar/test.c
@@ -11,14 +11,14 @@ int main(int argc, string argv[])
 
     int len = strlen(key);
 
-    int small_letters[26][26]; //to count number of letters
-    int big_letters[26][26];
+    char small_letters[26][26]; //to count number of letters
+    char big_letters[26][26];
 
     for (int i = 0, k = 0; i < 26; i++, k++)
     {
         for (int j = 65; j < 91; j++)
         {
-            big_letters[i] == j;
+            big_letters[i] = j;
             big_letters[k] = 0;
             small_letters[i] = j + 32;
             small_letters[k] = 0;
