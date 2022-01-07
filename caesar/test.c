18b6d9566d98fb35f1513475bffd775d3270f659
brucette <89015988+brucette@users.noreply.github.com>
2022-01-07 19:17:53 +0000

    Fri, Jan 7, 2022, 7:17 PM +00:00

--- caesar/test.c
+++ caesar/test.c
@@ -20,9 +20,9 @@ int main(int argc, string argv[])
         {
             //strcpy(&j, big_letters[i]);
             //strcpy(0, big_letters[k]);
-            big_letters[i][k] = 0;
-            strcpy(&j+32, small_letters[i]);
-            strcpy(0, small_letters[k]);
+            big_letters[i][k] = j;
+            //strcpy(&j+32, small_letters[i]);
+            //strcpy(0, small_letters[k]);
             //small_letters[i] = j + 32;
             //small_letters[k] = 0;
         }
