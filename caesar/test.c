7a77c6413b62692c7238fc7821fff6be9f6f491b
brucette <89015988+brucette@users.noreply.github.com>
2022-01-07 17:46:48 +0000

    Fri, Jan 7, 2022, 5:46 PM +00:00

--- caesar/test.c
+++ caesar/test.c
@@ -4,6 +4,37 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+
+int main(int argc, string argv[])
+{
+    if (argc != 2 || argv[1] != 26) //checks that 1 CL argument is given and its length is 26
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    string key = argv[1];
+
+    if (only_digits(argv[1]) == false)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    else
+    {
+        key = atoi(argv[1]);
+    }
+
+    string plain = get_string("plaintext:  ");
+
+    printf("ciphertext: ");
+    for (int i = 0, n = strlen(plain); i < n; i++)
+    {
+        printf("%c", rotate(plain[i], key));
+    }
+    printf("\n");
+}
+
 bool is_valid(string key)
 {
     int len = strlen(key);
