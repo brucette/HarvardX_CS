cf2dde4c335dc8d0274aed1ee01d5027846f0bee
brucette <89015988+brucette@users.noreply.github.com>
2022-01-07 17:43:33 +0000

    Fri, Jan 7, 2022, 5:43 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -48,12 +48,14 @@ bool is_valid(string key)
     int small_letters[26][26]; //to count number of letters
     int big_letters[26][26];
 
-    for (int i = 0; i < 26; i++)
+    for (int i = 0, k = 0; i < 26; i++, k++)
     {
         for (int j = 65; j < 91; j++)
         {
             big_letters[i] = j;
+            big_letters[k] = 0;
             small_letters[i] = j + 32;
+            small_letters[k] = 0;
         }
     }
 
