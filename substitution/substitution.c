41f751592f1e9bbb75e8dbf641c9f1c22521317e
brucette <89015988+brucette@users.noreply.github.com>
2022-01-07 20:34:17 +0000

    Fri, Jan 7, 2022, 8:34 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -77,7 +77,7 @@ bool is_valid(string key)
 
     for (int m = 0; m < len; m++)
 
-        if (key[m] == )
+        if (key[m] == big_letters[])
         {
             return false;
             printf("No duplicate characters allowed.");
