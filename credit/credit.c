fb1d3fbb29054f178cfcb24fccef2dcc6d54b03c
brucette <89015988+brucette@users.noreply.github.com>
2022-01-09 19:38:02 +0000

    Sun, Jan 9, 2022, 7:38 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,1 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
 
+
+int main(void)
+{
+    long number = get_long("Number ");
+
+    
+
+    printf("AMEX\n");
+    printf("MASTERCARD\n");
+    printf("VISA\n");
+    printf("INVALID\n");
+}
