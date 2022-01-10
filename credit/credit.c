a28afbff6d4dd9e12964d104808e3643ae326ef7
brucette <89015988+brucette@users.noreply.github.com>
2022-01-10 16:24:45 +0000

    Mon, Jan 10, 2022, 4:24 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,8 +6,9 @@ int main(void)
 {
     long number = get_long("Number: ");
 
-    printf("%li\n", number % 30);
+    printf("%li\n", number % 30);//HOW IS THIS SUPPOSED TO WORK???
 
+    char characters[]
     printf("AMEX\n");
     printf("MASTERCARD\n");
     printf("VISA\n");
