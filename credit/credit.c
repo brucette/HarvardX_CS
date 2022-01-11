b0794c38a149368035cab6dc3c71a10addf2fc42
brucette <89015988+brucette@users.noreply.github.com>
2022-01-11 11:45:36 +0000

    Tue, Jan 11, 2022, 11:45 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,7 +12,7 @@ int main(void)
     long number = get_long("Number: ");
     int digit;
     int remainder_sum = 0;
-    char *backwards = NULL;
+    char *backwards[sizeof(number)];
 
     //
     int counter = 0;
