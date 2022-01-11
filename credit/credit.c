06c6cc62404e277c44b56494ae94889c8705bbba
brucette <89015988+brucette@users.noreply.github.com>
2022-01-11 11:14:32 +0000

    Tue, Jan 11, 2022, 11:14 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -22,7 +22,7 @@ int main(void)
         if (counter % 2 != 0)
         {
             printf("backwards: %i\n", digit);       // get every other digit starting from second last
-            printf("char: %c", (char) digit);
+            printf("char: %c\n", (char) digit);
             //strcpy(backwards, &conv);
         }
         else
