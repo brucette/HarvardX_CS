bcbfb6edec1a97d81e2284f4b3126f623b68b807
brucette <89015988+brucette@users.noreply.github.com>
2022-01-12 14:13:47 +0000

    Wed, Jan 12, 2022, 2:13 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -137,7 +137,7 @@ bool vote(int voter, int rank, string name)
         if (strcmp(name, candidates[i].name) == 0)
         {
             candidates[i].votes++;
-            preferences[voter][rank];
+            preferences[voter][rank] = ;
             return true;
         }
     }
