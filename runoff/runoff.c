d96a8a93731c633063c13c73610ee5405605f55d
brucette <89015988+brucette@users.noreply.github.com>
2022-01-12 14:17:33 +0000

    Wed, Jan 12, 2022, 2:17 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,7 @@ bool vote(int voter, int rank, string name)
     // TODO
     for (int i = 0; i < voter_count; i++)
     {
-        for (int j = 0; j < candidate_count;)
+        for (int j = 0; j < candidate_count; j++)
         {
 
         }
