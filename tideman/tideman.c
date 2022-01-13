26007cc26bfc10ce88b51b35ce1a7466efb01e39
brucette <89015988+brucette@users.noreply.github.com>
2022-01-13 10:44:22 +0000

    Thu, Jan 13, 2022, 10:44 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -104,7 +104,7 @@ bool vote(int rank, string name, int ranks[])
     {
         if (strcmp(name, candidates[i]) == 0) //compare candidate voter gave to all the candidates running
         {
-            ranks[rank] = 
+            ranks[rank] =
             return true;
         }
     }
