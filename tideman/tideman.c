7e39804efdf862570829458a4b305f2d04b8315d
brucette <89015988+brucette@users.noreply.github.com>
2022-01-13 11:48:33 +0000

    Thu, Jan 13, 2022, 11:48 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -118,8 +118,10 @@ void record_preferences(int ranks[])
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; )
-        preferences[i] = ranks[i];
+        for (int j = 0; j < candidate_count; j++)
+        {
+            preferences[i] = ranks[i];
+        }
     }
     return;
 }
