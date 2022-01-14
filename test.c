6a5622b7febbc7470f90e2dc43e6b4d0a4ad2bf4
brucette <89015988+brucette@users.noreply.github.com>
2022-01-14 13:53:19 +0000

    Fri, Jan 14, 2022, 1:53 PM +00:00

--- test.c
+++ test.c
@@ -7,9 +7,9 @@
 void record_preferences(int ranks[])
 {
     // TODO
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = 0; j < 3; j++)
         {
             if (ranks[i] == preferences[j])
             {
