d30c7e4387336bc31633623eb9f76350a82c1359
brucette <89015988+brucette@users.noreply.github.com>
2022-01-14 13:51:44 +0000

    Fri, Jan 14, 2022, 1:51 PM +00:00

---
+++ test.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// Max number of candidates
+#define MAX 3
+
+void record_preferences(int ranks[])
+{
+    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (ranks[i] == preferences[j])
+            {
+                preferences[i][j]++
+            }
+        }
+    }
+    return;
+}
+
+// ranks[i] is voter's ith preference
+int ranks[] = {2, 1, 0};
+
+int preferences[3][3];
+
+int main(void)
+{
+
+    record_preferences(ranks);
+
+
+}
