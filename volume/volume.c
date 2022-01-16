598004eb292d734395dae205bb0475bdfafed3eb
brucette <89015988+brucette@users.noreply.github.com>
2022-01-16 15:22:41 +0000

    Sun, Jan 16, 2022, 3:22 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -45,7 +45,7 @@ int main(int argc, char *argv[])
     // TODO: Read samples from input file and write updated data to output file
     // allocate memory for 2 int sample
     int16_t temp_sample;
-    
+
     // copy a sample from file to memory
     fread();
 
