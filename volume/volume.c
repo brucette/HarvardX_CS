e529af71f2544a8b3efdfd905d2208fbcb8bb131
brucette <89015988+brucette@users.noreply.github.com>
2022-01-16 15:15:23 +0000

    Sun, Jan 16, 2022, 3:15 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,8 +35,8 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     int temp_header[HEADER_SIZE];   //allocate memory for header
-    fread(&temp_header, sizeof(HEADER_SIZE), 1, output);    //read header to memory
-    fwrite(&temp_header, sizeof(HEADER_SIZE), 1); //write header from memory to new file
+    fread(&temp_header, sizeof(HEADER_SIZE), 1, input);    //read header to memory
+    fwrite(&temp_header, sizeof(HEADER_SIZE), 1, output); //write header from memory to new file
 
     // TODO: Read samples from input file and write updated data to output file
 
