5477a8aad0165c0bdf01be2e9904586bf40ee23e
brucette <89015988+brucette@users.noreply.github.com>
2022-01-17 15:55:57 +0000

    Mon, Jan 17, 2022, 3:55 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -6,16 +6,18 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     int sum = 0;
     int grey = 0;
-    int average = round(sum / 3);
-    if (average > 255)
-    {
-        average = 255;
-    }
+
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             sum += image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed;
+            int average = round(sum / 3);
+            if (average > 255)
+            {
+                average = 255;
+            }
             image[i][j].rgbtBlue = average;
             image[i][j].rgbtGreen = average;
             image[i][j].rgbtRed = average;
