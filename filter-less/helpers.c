bec5f8c28f60b47af63d942603d5d84b2789a346
brucette <89015988+brucette@users.noreply.github.com>
2022-01-17 16:19:21 +0000

    Mon, Jan 17, 2022, 4:19 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -30,6 +30,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
             int sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
             int sepiaBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+
             if (sepiaRed > 255)
             {
                 sepiaRed = 255;
@@ -42,6 +43,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             {
                 sepiaBlue = 255;
             }
+
             image[i][j].rgbtBlue = sepiaBlue;
             image[i][j].rgbtGreen = sepiaGreen;
             image[i][j].rgbtRed = sepiaRed;
