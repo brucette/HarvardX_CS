7da6aa721e33f421f5bed9c0dc064e586258f099
brucette <89015988+brucette@users.noreply.github.com>
2022-01-17 20:11:01 +0000

    Mon, Jan 17, 2022, 8:11 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -7,7 +7,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int avg = 
+            int avg = round((float)image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed / 3);
+            image[i][j].rgbtBlue = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtRed = avg;
         }
     }
     return;
