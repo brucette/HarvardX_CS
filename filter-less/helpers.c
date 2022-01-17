e3d3beda536c17a486f901d4a8de0baeaaccfcb7
brucette <89015988+brucette@users.noreply.github.com>
2022-01-17 15:58:56 +0000

    Mon, Jan 17, 2022, 3:58 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -5,9 +5,7 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     int sum = 0;
-    int grey = 0;
-
-
+    
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
