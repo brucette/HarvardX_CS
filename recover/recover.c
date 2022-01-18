ae78c6bc89f075d2a3910183a86f2fb229f120d9
brucette <89015988+brucette@users.noreply.github.com>
2022-01-18 18:26:57 +0000

    Tue, Jan 18, 2022, 6:26 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     FILE *card = fopen(argv[1],"r");
 
     // keep reading 512 byte chunks into a buffer
-    while (fread(buffer))
+    while (fread(buffer, ))
         // check if first 4 bytes are start of a jpg
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0 == 0xe0))
         {
