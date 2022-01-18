d3b0fc0483e1e687ac1b4b04a6deb5ed71fca146
brucette <89015988+brucette@users.noreply.github.com>
2022-01-18 17:37:32 +0000

    Tue, Jan 18, 2022, 5:37 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -14,6 +14,10 @@ int main(int argc, char *argv[])
     // keep reading 512 byte chunks into a buffer
 
         // check if first 4 bytes are start of a jpg
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0 == 0xe0))
+        {
+
+        }
 
         // if so, copy byte into a new file name ###.jpg
 }
