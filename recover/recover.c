66935991bf952eb9d6309ce3723a60af379a5dd3
brucette <89015988+brucette@users.noreply.github.com>
2022-01-18 17:32:56 +0000

    Tue, Jan 18, 2022, 5:32 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -10,4 +10,10 @@ int main(int argc, char *argv[])
     }
 
     //open file for reading ("coping")
+
+    // keep reading 512 byte chunks into a buffer
+
+        // check if first 4 bytes are start of a jpg
+
+        // if so, copy byte into a new file name ###.jpg
 }
