c2c7703c33a2f8f090e12847d7365431a5a1b78b
brucette <89015988+brucette@users.noreply.github.com>
2022-01-18 18:33:03 +0000

    Tue, Jan 18, 2022, 6:33 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,14 +21,14 @@ int main(int argc, char *argv[])
     // keep reading 512 byte chunks into a buffer
     while (fread(buffer, sizeof(buffer), 1, card))
     {
-
-        
-    }
         // check if first 4 bytes are start of a jpg
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0 == 0xe0))
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0 == 0xe0) && counter )
         {
 
         }
+
+    }
+
 
         // if so, copy byte into a new file name ###.jpg
         sprintf(filename, "%03i.jpg", no.(counter))
