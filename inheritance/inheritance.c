4d4a719ccb4f85be9f5599f8fdccf6f828588334
brucette <89015988+brucette@users.noreply.github.com>
2022-01-20 14:12:00 +0000

    Thu, Jan 20, 2022, 2:12 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -80,7 +80,7 @@ void free_family(person *p)
     // TODO: Handle base case
     if (p == NULL)
     {
-        return 1;
+        return;
     }
     // TODO: Free parents recursively
     free_family(p->parent[0]->parents);
