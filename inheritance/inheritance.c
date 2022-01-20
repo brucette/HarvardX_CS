00fd504d6e1e267bb2c3f578f080bb228ea3cc4a
brucette <89015988+brucette@users.noreply.github.com>
2022-01-20 14:17:36 +0000

    Thu, Jan 20, 2022, 2:17 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -83,10 +83,8 @@ void free_family(person *p)
         return;
     }
     // TODO: Free parents recursively
-    free_family(p->parents[0]->parents[0]);
-    free_family(p->parents[0]->parents[1]);
-    free_family(p->parents[1]->parents[0]);
-    free_family(p->parents[1]->parents[1]);
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
 
     // TODO: Free child
     free(p);
