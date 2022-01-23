e10594fe8a39b9de7727e279f0d5c497c2ae3e3d
brucette <89015988+brucette@users.noreply.github.com>
2022-01-23 18:33:07 +0000

    Sun, Jan 23, 2022, 6:33 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -47,9 +47,11 @@ unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
     return toupper(word[0]) - 'A';
-    //(word[0] + word[1]) + 546 % N;  676;
+    //(word[0] - 'A' * 26) + (word[1] - 'a') % N;  676;
     // Az 65 122
+    (0) + ()
     // Ba 66 97
+    //
 }
 
 // Loads dictionary into memory, returning true if successful, else false
