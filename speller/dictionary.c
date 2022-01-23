66816e83d4e2bf7417aaef432b41442e3d3969be
brucette <89015988+brucette@users.noreply.github.com>
2022-01-23 15:10:44 +0000

    Sun, Jan 23, 2022, 3:10 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -101,7 +101,7 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return counter;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
