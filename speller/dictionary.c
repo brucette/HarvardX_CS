9a49e9c7739eecd54d09535d22f884fcc45b8db8
brucette <89015988+brucette@users.noreply.github.com>
2022-01-24 17:22:25 +0000

    Mon, Jan 24, 2022, 5:22 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -70,9 +70,9 @@ bool load(const char *dictionary)
     char buffer[LENGTH + 1];
 
     // read strings from the file one at a time
-    while (!EOF) // will return EOF once it reaches the end
+    while (fscanf(dict, "%s", buffer) != EOF) // will return EOF once it reaches the end
     {
-        fscanf(dict, "%s", buffer); // create a new node for each word
+         // create a new node for each word
         node *n = malloc(sizeof(node));
         if (n == NULL)
         {
