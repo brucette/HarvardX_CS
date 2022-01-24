d376b7672196bdea62ed5743fc33c4063d8df334
brucette <89015988+brucette@users.noreply.github.com>
2022-01-24 12:09:38 +0000

    Mon, Jan 24, 2022, 12:09 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -70,6 +70,14 @@ bool load(const char *dictionary)
     // read strings from the file one at a time
     while (fscanf(file, "%s", buffer)) // will return EOF once it reaches the end
     {
+        // create a new node for each word
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return false;
+        }
+        counter++;
+
         // hash word to obtain a hash value
         int index = hash(buffer);
 
@@ -78,24 +86,15 @@ bool load(const char *dictionary)
         {
             strcpy(table[index]->word, buffer);
             table[index]->next = NULL;
-            counter++;
+
         }
         else
-        {*/
-        // create a new node for each word
-        node *n = malloc(sizeof(node));
-        if (n == NULL)
         {
-            return false;
+            // copy buffer into node
+            strcpy(n->word, buffer);
+            n = table[index];
+            table[index]->next = n;
         }
-
-        // copy buffer into node
-        strcpy(n->word, buffer);
-        n->next = NULL;
-        counter++;
-        n = table[index];
-        table[index]->next = n;
-    }
 return true;
 }
 
