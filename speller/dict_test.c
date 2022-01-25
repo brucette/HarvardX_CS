a027bd851f25c0f6a89ca3689e1c7bbff1333017
brucette <89015988+brucette@users.noreply.github.com>
2022-01-25 14:17:06 +0000

    Tue, Jan 25, 2022, 2:17 PM +00:00

--- speller/dict_test.c
+++ speller/dict_test.c
@@ -5,7 +5,7 @@ int main(void)
     FILE *file = fopen("hi.txt", "r");
     if (file != NULL)
     {
-        char buffer[3];
+        char buffer[5];
         fscanf(file, "%s", buffer);
         fclose(file);
         printf("%s\n", buffer);
