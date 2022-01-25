bb6fba7637ed3720ae94ad711d4c5a826e7fc23f
brucette <89015988+brucette@users.noreply.github.com>
2022-01-25 09:03:45 +0000

    Tue, Jan 25, 2022, 9:03 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -70,7 +70,7 @@ bool load(const char *dictionary)
     }
 
     char buffer[LENGTH + 1];
-    printf("buffer is %s\n", buffer);
+
 
     // read strings from the file one at a time
     while (!EOF) // will return EOF once it reaches the end
@@ -86,7 +86,7 @@ bool load(const char *dictionary)
 
         // hash word to obtain a hash value
         unsigned int index = hash(buffer);
-
+        printf("buffer is %s\n", buffer);
 
         // copy buffer into node
         strcpy(n->word, buffer);
@@ -97,6 +97,7 @@ bool load(const char *dictionary)
         //printf("table index is %i%s\n", index, n->word);
         table[index] = n;
     }
+    printf("buffer is %s\n", buffer);
     fclose(dict);
     return true;
 }
