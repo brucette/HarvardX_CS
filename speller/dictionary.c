eb83144836ee611bc00e3c243e6599d6458df9e0
brucette <89015988+brucette@users.noreply.github.com>
2022-01-25 14:27:00 +0000

    Tue, Jan 25, 2022, 2:27 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -125,8 +125,9 @@ bool unload(void)
         {
             node *tmp = cursor;
             cursor = cursor->next;
-            free(tmp);
+            // free(tmp);
             tmp = cursor;
+            printf("loop");
         }
     }
     return true;
