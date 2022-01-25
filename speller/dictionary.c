f3127393ee01a0c58a3d1d7db0e26114235f437b
brucette <89015988+brucette@users.noreply.github.com>
2022-01-25 14:24:10 +0000

    Tue, Jan 25, 2022, 2:24 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -120,9 +120,10 @@ bool unload(void)
     for (int i = 0; i < N; i++)
     {
         node *cursor = table[i];
-        node *tmp = cursor;
+
         while (cursor != NULL)
         {
+            node *tmp = cursor;
             cursor = cursor->next;
             free(tmp);
             tmp = cursor;
