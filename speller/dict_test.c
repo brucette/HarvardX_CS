9a104d14b4b9a75ab9a338425c9d356512210b41
brucette <89015988+brucette@users.noreply.github.com>
2022-01-25 14:04:18 +0000

    Tue, Jan 25, 2022, 2:04 PM +00:00

--- speller/dict_test.c
+++ speller/dict_test.c
@@ -1,1 +1,16 @@
-#include "dictionary.c"
+
+/ Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+
+// TODO: Choose number of buckets in hash table
+const unsigned int N = 676;
+
+// Hash table
+node *table[N];
+
+unsigned int counter = 0;
