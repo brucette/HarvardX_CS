aeae1812f860741b8ac071be2ce0bd0c1b1fa6b6
brucette <89015988+brucette@users.noreply.github.com>
2022-01-28 13:53:30 +0000

    Fri, Jan 28, 2022, 1:53 PM +00:00

--- world-cup/test.py
+++ world-cup/test.py
@@ -7,6 +7,9 @@ teams = []
 # TODO: Read teams into memory from file
 with open(sys.argv[1]) as file:
     reader = csv.DictReader(file)
+    next(reader)
+    for row in reader:
+        rating = row[1]
 
     teams.append(reader[int(ratings)])
     print(reader[ratings])
