4923140849636a685855ed1cca3e0cae91e922ea
brucette <89015988+brucette@users.noreply.github.com>
2022-01-28 09:49:56 +0000

    Fri, Jan 28, 2022, 9:49 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -5,15 +5,17 @@ while True:
     if height > 0 and height < 9:
         break
 
+counter = 1
 
 for i in range(height):
     for j in range(height - 1):
-        print(".", end="")
+        print(" ", end="")
 
-    for k in range():
+    for k in range(counter):
         print("#", end="")
-        
+
     print()
     height -= 1
+    counter += 1
 
 
