1489b8846f2df606d4085ef9e98cb7d65e2a69ee
brucette <89015988+brucette@users.noreply.github.com>
2022-01-28 13:37:19 +0000

    Fri, Jan 28, 2022, 1:37 PM +00:00

--- world-cup/test.py
+++ world-cup/test.py
@@ -8,7 +8,8 @@ teams = []
 with open(sys.argv[1]) as file:
     reader = csv.DictReader(file)
     teams.append(reader)
-    print(reader)
+    for i in teams:
+        print(i)
 
 
 #countries = {"Norway":"1915"}
