964c28d2017378d2d04dd1c166b9a5df4802a9a4
brucette <89015988+brucette@users.noreply.github.com>
2022-01-28 13:42:00 +0000

    Fri, Jan 28, 2022, 1:42 PM +00:00

--- world-cup/test.py
+++ world-cup/test.py
@@ -7,9 +7,8 @@ teams = []
 # TODO: Read teams into memory from file
 with open(sys.argv[1]) as file:
     reader = csv.DictReader(file)
-    teams.append(reader)
-    for i in teams:
-        print(i)
+    teams.append(reader[int(ratings)])
+    print(reader[ratings])
 
 
 #countries = {"Norway":"1915"}
