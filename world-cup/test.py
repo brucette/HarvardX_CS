5593e5e9e4f2636923423ffaf4a5133cef75969a
brucette <89015988+brucette@users.noreply.github.com>
2022-01-28 13:59:41 +0000

    Fri, Jan 28, 2022, 1:59 PM +00:00

--- world-cup/test.py
+++ world-cup/test.py
@@ -6,8 +6,7 @@ import random
 teams = []
 # TODO: Read teams into memory from file
 with open(sys.argv[1]) as file:
-    reader = csv.DictReader(file)
-    next(reader)
+    reader = csv.DictReader(file) #gives a dictionary
     rating = reader[1]
 
     teams.append(reader[int(ratings)])
