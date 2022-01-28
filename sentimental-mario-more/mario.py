88225c8d17e5ad5a8348487059473f2e2cf4c7d1
brucette <89015988+brucette@users.noreply.github.com>
2022-01-28 10:12:12 +0000

    Fri, Jan 28, 2022, 10:12 AM +00:00

---
+++ sentimental-mario-more/mario.py
@@ -0,0 +1,15 @@
+from cs50 import get_int
+
+# Keep asking for integer between 1-8 if user enters something other than that
+while True:
+    height = get_int("Height:")
+    if height > 0 and height < 9:
+        break
+
+counter = 1
+
+for i in range(height):
+    for j in range(height - 1):
+        print(".", end="")
+
+        
