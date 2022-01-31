afeb8212284e79a45082302e99a56fe43ccd0274
brucette <89015988+brucette@users.noreply.github.com>
2022-01-31 16:51:47 +0000

    Mon, Jan 31, 2022, 4:51 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -32,7 +32,7 @@ def main():
 
     for j in range(N):
         winner = simulate_tournament(teams)
-        counts[winner] += 1
+        #counts[winner] += 1
 
 
     # Print each team's chances of winning, according to simulation
