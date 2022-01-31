402161c92aeec5d2435227dea2c75a905f6cb21e
brucette <89015988+brucette@users.noreply.github.com>
2022-01-31 13:45:24 +0000

    Mon, Jan 31, 2022, 1:45 PM +00:00

--- world-cup/test.py
+++ world-cup/test.py
@@ -8,8 +8,8 @@ teams = []
 with open(sys.argv[1]) as file:
     reader = csv.DictReader(file) #gives a dictionary
     for row in reader:
-        newdict = row["team"], int(row["rating"])
-        teams.append(newdict)
+        #newdict = row["team"], int(row["rating"])
+        teams.append(row)
 
 for i in teams:
     print(teams["team"]) #newdict[0])
