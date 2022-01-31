17844a6f7c3a0c45f2ba39da4a96bfb368f09964
brucette <89015988+brucette@users.noreply.github.com>
2022-01-31 16:07:16 +0000

    Mon, Jan 31, 2022, 4:07 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -26,6 +26,7 @@ def main():
     # TODO: Simulate N tournaments and keep track of win counts
     for i in teams:
         counts["team"] = i["team"]
+        counts[team] = 0;
 
     for i in range(N):
         winner = simulate_tournament(teams)
