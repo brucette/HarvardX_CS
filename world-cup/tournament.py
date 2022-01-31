ad234993873c8ff9f56e224e09d1475e8979f52c
brucette <89015988+brucette@users.noreply.github.com>
2022-01-31 14:08:16 +0000

    Mon, Jan 31, 2022, 2:08 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -19,7 +19,7 @@ def main():
     with open(sys.argv[1]) as file:
       reader = csv.DictReader(file)
       for row in reader:
-          int(row["rating"])
+          int(row)
           teams.append(row)
 
     counts = {}
