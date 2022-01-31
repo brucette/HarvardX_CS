da18ace7e7c2ae5a31f81a536410a7590ffde033
brucette <89015988+brucette@users.noreply.github.com>
2022-01-31 13:28:20 +0000

    Mon, Jan 31, 2022, 1:28 PM +00:00

--- world-cup/test.py
+++ world-cup/test.py
@@ -12,30 +12,8 @@ with open(sys.argv[1]) as file:
         teams.append(newdict)
         print(newdict[0])
 
-def simulate_game(team1, team2):
-    """Simulate a game. Return True if team1 wins, False otherwise."""
-    rating1 = team1["rating"]
-    rating2 = team2["rating"]
-    probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))
-    return random.random() < probability
-
-def simulate_round(teams):
-    """Simulate a round. Return a list of winning teams."""
-    winners = []
-
-    # Simulate games for all pairs of teams
-    for i in range(0, len(teams), 2):
-        if simulate_game(teams[i], teams[i + 1]):
-            winners.append(teams[i])
-        else:
-            winners.append(teams[i + 1])
-
-    return winners
 
 
-while len(simulate_round(teams)) != 1:
-     winner = (simulate_round(teams))
-print(winner)
 
 
 
