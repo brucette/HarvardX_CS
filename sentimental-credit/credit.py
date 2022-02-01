b6aaddfc2bcd1a6b04f8a123b7358732b69849cb
brucette <89015988+brucette@users.noreply.github.com>
2022-02-01 14:09:30 +0000

    Tue, Feb 1, 2022, 2:09 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,7 +6,7 @@ length = len(card)
 
 if length != 13 and length != 15 and length != 16:
     print("INVALID\n")
-print(length)
+#print(length)
 
 # String for every other digit starting from second last
 every_other = ""
@@ -16,7 +16,7 @@ while i >= 0:
     every_other += card[i]
     i -= 2
 
-print(every_other)
+#print(every_other)
 
 # String for every other digit (from second last) multiplied by 2
 multiplied = ""
@@ -24,7 +24,7 @@ multiplied = ""
 for digit in every_other:
     multiplied += str(int(digit) * 2)
 
-print(multiplied)
+#print(multiplied)
 
 # Sum of products of every other digit starting from second last multiplied by 2
 sum = 0
@@ -32,7 +32,7 @@ sum = 0
 for digit in multiplied:
     sum += int(digit)
 
-print(sum)
+#print(sum)
 
 # Sum of remaining numbers
 sum_rest = 0
@@ -45,7 +45,7 @@ while i >= 0:
 print(sum_rest + sum)
 
 if sum_rest + sum % 10 == 0:
-    if card[0] == 4 and (length == 13 or length == 16):
+    if card[0] == 4 and length == 13 or length == 16):
         print("VISA\n")
 
     if card[0] == 5 and (card[1] >= 1 and card[1] <= 5) and length == 16:
