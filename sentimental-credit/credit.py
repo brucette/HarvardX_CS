2e63cd1d2113fe419cbfb49e25bc3afb2bf9e56a
brucette <89015988+brucette@users.noreply.github.com>
2022-02-01 14:34:17 +0000

    Tue, Feb 1, 2022, 2:34 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,9 +4,6 @@ from cs50 import get_string
 card = get_string("Number: ")
 length = len(card)
 
-if length != 13 and length != 15 and length != 16:
-    print("INVALID\n")
-#print(length)
 
 # String for every other digit starting from second last
 every_other = ""
