8b80b84ee8f78aaaba14fbd5f20d073353a363d4
brucette <89015988+brucette@users.noreply.github.com>
2022-02-01 16:39:02 +0000

    Tue, Feb 1, 2022, 4:39 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -12,5 +12,6 @@ words = 0
 # Number of sentences
 sentences = 0
 
-# 0.0588 * L - 0.296 * S - 15.8, where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text
+# 0.0588 * L - 0.296 * S - 15.8,
+# where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text
 
