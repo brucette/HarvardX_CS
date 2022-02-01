8021149d9282ee24b025aa9983bd64061af1cae7
brucette <89015988+brucette@users.noreply.github.com>
2022-02-01 14:43:55 +0000

    Tue, Feb 1, 2022, 2:43 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -43,7 +43,7 @@ if (sum_rest + sum) % 10 == 0:
     if card[0] == '4' and (length == 13 or 16):
         print("VISA\n")
 
-    elif card[0] == '5' and card[1] >= '1' and card[1] <= '5' and length == 16:
+    elif card[0] == '5' and (card[1] >= '1' and <= '5') and length == 16: #card[1]
         print("MASTERCARD\n")
 
     elif card[0] == '3' and (card[1] == '4' or '7') and length == 15:   #or card[1] ==
