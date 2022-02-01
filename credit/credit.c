a5b78698383c67a75af3b0590a2ddaa9c43c4aea
brucette <89015988+brucette@users.noreply.github.com>
2022-02-01 15:07:26 +0000

    Tue, Feb 1, 2022, 3:07 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -14,8 +14,7 @@ int main(void)
     int remainder_sum = 0;
     int first_set[8];
     long backwards[sizeof(number)];
-    string num = (string) number;
-    printf("%s", number)
+
     /*
     int counter = 0;
     do
@@ -59,7 +58,7 @@ int main(void)
 
 
 
-    /*int multiplier = 100;
+    int multiplier = 100;
     int last_numbs;
     int first_numb;
     int sum = 0;*/
