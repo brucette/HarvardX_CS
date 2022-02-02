fa7bb3cd60c09ebfd202cc33cb5db30ab66c32f7
brucette <89015988+brucette@users.noreply.github.com>
2022-02-02 17:59:43 +0000

    Wed, Feb 2, 2022, 5:59 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -26,7 +26,6 @@ def main():
     # TODO: Find longest match of each STR in DNA sequence
     # len(s) might be useful
     # s[i:j] finds substring
-    
     # TODO: Check database for matching profiles
 
     return
