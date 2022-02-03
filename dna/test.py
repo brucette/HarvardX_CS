9bb949a37d0af5a2ef09694256b51a9d7c0ec371
brucette <89015988+brucette@users.noreply.github.com>
2022-02-03 09:57:48 +0000

    Thu, Feb 3, 2022, 9:57 AM +00:00

--- dna/test.py
+++ dna/test.py
@@ -15,18 +15,18 @@ dna = [] #argv[2]
 with open(sys.argv[1], "r") as file:
     reader = csv.DictReader(file) #gives a dictionary
     #header = csv.reader(file)
-    for row in reader:
-        database.append(row)
-
     header = dict(list(reader)[0])
     #dict_from_csv = dict(list(csv_reader)[0])
     list_of_column_names = list(header.keys())
+
+    for row in reader:
+        database.append(row)
 
 print()
 #for i in range(len(snippets)):
     #print(snippets[i])
 print(list_of_column_names)
-#print(database)
+print(database)
 
 
 
