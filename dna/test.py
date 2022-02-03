c89c67fc34857d0ed0d4484f9c53082c14b6788a
brucette <89015988+brucette@users.noreply.github.com>
2022-02-03 14:23:12 +0000

    Thu, Feb 3, 2022, 2:23 PM +00:00

--- dna/test.py
+++ dna/test.py
@@ -34,14 +34,14 @@ with open(sys.argv[2], "r") as f:
 
 print()
 
-#print(f"genes: {genes}")
+print(f"genes: {genes}")
 #print(type(genes))
 #print(f"dna: {dna}")
 #print(type(dna))
 #print(f"STRs: {STRs}")
 #print(f"Snippets: {snippets}")
 #print(f"Reader: {reader}")
-print(f"database: {database}")
+#print(f"database: {database}")
 #print(f"headers: {header}")
 #for i in range(1, len(list_of_column_names)):
     #print(list_of_column_names[i])
