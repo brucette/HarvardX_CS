5976972b9652cffbbb06b5a883c0dad8ca72d6ed
brucette <89015988+brucette@users.noreply.github.com>
2022-02-04 14:22:21 +0000

    Fri, Feb 4, 2022, 2:22 PM +00:00

--- dna/test.py
+++ dna/test.py
@@ -70,9 +70,8 @@ print()
 print(database)
 
 for entry in database:
-    #print(entry.items())
+    print(f"***,{entry.items()}")
     for entry_k, entry_v in entry.items():
-        print(f"***,{entry.items()}")
         if entry_k in snippets.keys():
             if int(entry_v) == snippets[entry_k]:
                 print(entry["name"])
