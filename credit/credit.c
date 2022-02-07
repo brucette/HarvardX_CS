0df21688df7818d9b1422b5b7ee1afc3b98078ae
brucette <89015988+brucette@users.noreply.github.com>
2022-02-07 17:51:06 +0000

    Mon, Feb 7, 2022, 5:51 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -60,7 +60,7 @@ int main(void)
     //ADD ALL DIGITS IN first_set together
     for (int i = 0; i < strlen(backwards); i++)
     {
-        int again = atoi(backwards[i]);
+        int again = atoi(&backwards[i]);
         back_sum += again;
 
         printf("BACKSUM: %i\n", back_sum);
