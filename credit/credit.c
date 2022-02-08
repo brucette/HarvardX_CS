f9419fd88d5ef56f69b30b1fb66ac18f866d141e
brucette <89015988+brucette@users.noreply.github.com>
2022-02-08 10:04:26 +0000

    Tue, Feb 8, 2022, 10:04 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,9 +5,6 @@
 #include <stdlib.h>
 
 // MASTERCARD: 16 digits, Start with 51/52/53/54/55
-// VISA: 13/16 digits, Start with 4
-// AMEX: 15 digits, Start with 34/37
-
 
 int main(void)
 {
@@ -15,7 +12,6 @@ int main(void)
 
     char card[17];
     char converted = sprintf(card, "%li", number);
-    //printf("*****STRING: %s\n", card);
 
     int digit;
     int remainder_sum = 0;
@@ -37,7 +33,7 @@ int main(void)
             // Multiply every other number starting from second last by 2
             int num = digit * 2;
 
-            // Convert that integer to string                   //printf("\n CHAR: %s\n", first_set);
+            // Convert that integer to string
             char conv = sprintf(first_set, "%i", num);
 
             // Concatenate that string to a new variable
@@ -70,13 +66,13 @@ int main(void)
     int final = back_sum + remainder_sum;
     printf("FINALSUM: %i\n", final);
 
-    // CHECK FOR LENGTH AND FIRST TWO CHARACTERS
     int length = strlen(card);
     //printf("CARDlength: %i\n", length);
 
     // Check if 0 exists in end sum by using % 10
     if (final % 10 == 0)
     {
+        // Check for valid length and first two characters
         if (card[0] == '4' && (length == 13 || length == 16))
         {
             printf("VISA\n");
@@ -97,11 +93,6 @@ int main(void)
 
 }
 
-
-
-
-
-    // 371449635398431
 
 
 
