c0d12cb76c26d88ac96d742b545ea3038dbf869c
brucette <89015988+brucette@users.noreply.github.com>
2022-02-09 17:08:41 +0000

    Wed, Feb 9, 2022, 5:08 PM +00:00

--- songs/3.sql
+++ songs/3.sql
@@ -1,6 +1,1 @@
-
-Te Bote - Remix                                         | 417920      |
-| SICKO MODE                                              | 312820      |
-| Walk It Talk It                                         | 276147      |
-| Him & I (with Halsey)                                   | 268867      |
-| Perfect
+SELECT name FROM songs ORDER BY duration_ms DESC LIMIT 5;
