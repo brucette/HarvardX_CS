b8aaf9c8677e30479138a61fc762d60321e7df5d
brucette <89015988+brucette@users.noreply.github.com>
2022-02-10 17:51:31 +0000

    Thu, Feb 10, 2022, 5:51 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -4,4 +4,4 @@ In 8.sql, write a SQL query to list the names of all people who starred in Toy S
     You may assume that there is only one movie in the database with the title Toy Story.
 
 /* gets name of star                                          gives Toy STory's ID */
-SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id = (SELECT id FROM movies WHERE title = "Toy Story");
+SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE movies.id = (SELECT id FROM movies WHERE title = "Toy Story");
