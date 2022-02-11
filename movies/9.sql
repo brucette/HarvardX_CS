0901a886454553400fdd3c91b0ec768114df58ae
brucette <89015988+brucette@users.noreply.github.com>
2022-02-11 08:56:36 +0000

    Fri, Feb 11, 2022, 8:56 AM +00:00

--- movies/9.sql
+++ movies/9.sql
@@ -4,3 +4,4 @@ In 9.sql, write a SQL query to list the names of all people who starred in a mov
     People with the same birth year may be listed in any order.
     No need to worry about people who have no birth year listed, so long as those who do have a birth year are listed in order.
     If a person appeared in more than one movie in 2004, they should only appear in your results once.
+SELECT name FROM people JOIN stars ON people.id = person_id JOIN movies ON movie_id = movies.id IN (SELECT id FROM movies where year = 2004);
