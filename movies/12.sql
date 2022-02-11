9fae17d30ec0143d10c2bd0c1ca4e3237f8ded90
brucette <89015988+brucette@users.noreply.github.com>
2022-02-11 16:03:24 +0000

    Fri, Feb 11, 2022, 4:03 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -4,7 +4,8 @@ In 12.sql, write a SQL query to list the titles of all movies in which both John
     You may assume that there is only one person in the database with the name Johnny Depp.
     You may assume that there is only one person in the database with the name Helena Bonham Carter.
 
-SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
+SELECT title FROM movies
+JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
 WHERE person_id = (SELECT id FROM people WHERE name IN ("Johnny Depp", "Helena Bonham Carter"));
 
 SELECT title, person_id FROM movies JOIN stars ON id = movie_id
