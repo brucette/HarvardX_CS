6e715dc8e36e3c89aa1a7e9f31faff33357d9e46
brucette <89015988+brucette@users.noreply.github.com>
2022-02-11 09:22:15 +0000

    Fri, Feb 11, 2022, 9:22 AM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -2,4 +2,7 @@ In 10.sql, write a SQL query to list the names of all people who have directed a
 
     Your query should output a table with a single column for the name of each person.
     If a person directed more than one movie that received a rating of at least 9.0, they should only appear in your results once.
-SELECT 
+
+
+/*           Get all movies with rating 9 or over */
+            (SELECT movie_if FROM ratings WHERE rating >= 9.0);
