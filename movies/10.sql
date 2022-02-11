17d9a19ff4806d54e7340ff81e08146e033e3a43
brucette <89015988+brucette@users.noreply.github.com>
2022-02-11 09:33:15 +0000

    Fri, Feb 11, 2022, 9:33 AM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -6,5 +6,4 @@ In 10.sql, write a SQL query to list the names of all people who have directed a
 
 /*           Get all movies with rating 9 or over */
 SELECT DISTINCT name FROM people JOIN directors ON people.id = directors.person_id JOIN ratings ON directors.movie_id = ratings.movie_id;
-WHERE
-            (SELECT movie_id FROM ratings WHERE rating >= 9.0);
+WHERE directors.movie_id IN (SELECT movie_id FROM ratings WHERE rating >= 9.0);
