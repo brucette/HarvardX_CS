e6bff6ee9315549e906e01d938f5957d7ec9b156
brucette <89015988+brucette@users.noreply.github.com>
2022-02-11 14:15:52 +0000

    Fri, Feb 11, 2022, 2:15 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -3,15 +3,20 @@ with the highest rated.
 
     Your query should output a table with a single column for the title of each movie.
     You may assume that there is only one person in the database with the name Chadwick Boseman.
-SELECT title FROM movies JOIN stars, ratings ON id = movie_id WHERE person_id = 1569276;
 
+SELECT title FROM movies JOIN stars ON id = stars.movie_id WHERE person_id = (SELECT id FROM people WHERE name = Chadwick Boseman);
+
+
+|          title           |
++--------------------------+
+| 42                       |
+| Ma Rainey's Black Bottom |
 | Message from the King    |
-   ...> | The Kill Hole            |
-   ...> | Black Panther            |
-   ...> | Draft Day                |
-   ...> | Gods of Egypt            |
-   ...> | Get on Up                |
-   ...> | Marshall                 |
-   ...> | 21 Bridges               |
-   ...> | Ma Rainey's Black Bottom |
-   ...> | 42 
+| The Kill Hole            |
+| Black Panther            |
+| Draft Day                |
+| Gods of Egypt            |
+| Get on Up                |
+| Marshall                 |
+| 21 Bridges               |
++--------------------------+
