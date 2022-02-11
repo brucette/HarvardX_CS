d6f851eede6577af1e87a3df826b7a8b715c8670
brucette <89015988+brucette@users.noreply.github.com>
2022-02-11 09:13:40 +0000

    Fri, Feb 11, 2022, 9:13 AM +00:00

--- movies/9.sql
+++ movies/9.sql
@@ -5,4 +5,4 @@ In 9.sql, write a SQL query to list the names of all people who starred in a mov
     If a person appeared in more than one movie in 2004, they should only appear in your results once.
 
 SELECT COUNT(DISTINCT name) FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id
-WHERE stars.movie_id IN (SELECT id FROM movies where year = 2004);
+WHERE stars.movie_id IN (SELECT id FROM movies where year = 2004) ORDER BY birth;
