e6db64e3cbbf976a2bfde7c5636f9e033c0132c3
brucette <89015988+brucette@users.noreply.github.com>
2022-02-11 14:23:20 +0000

    Fri, Feb 11, 2022, 2:23 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,24 +1,3 @@
-In 11.sql, write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting
-with the highest rated.
-
-    Your query should output a table with a single column for the title of each movie.
-    You may assume that there is only one person in the database with the name Chadwick Boseman.
-
-SELECT title, rating FROM movies JOIN stars ON id = stars.movie_id JOIN ratings ON id = ratings.movie_id
+SELECT title FROM movies JOIN stars ON id = stars.movie_id JOIN ratings ON id = ratings.movie_id
 WHERE person_id = (SELECT id FROM people WHERE name = 'Chadwick Boseman')
 ORDER BY rating DESC LIMIT 5;
-
-
-|          title           |
-+--------------------------+
-| 42                       |
-| Ma Rainey's Black Bottom |
-| Message from the King    |
-| The Kill Hole            |
-| Black Panther            |
-| Draft Day                |
-| Gods of Egypt            |
-| Get on Up                |
-| Marshall                 |
-| 21 Bridges               |
-+--------------------------+
