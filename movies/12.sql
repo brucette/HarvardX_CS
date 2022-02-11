fc1e3dbaf06f7a0bf9463a9a3a477f0a969803d9
brucette <89015988+brucette@users.noreply.github.com>
2022-02-11 17:13:23 +0000

    Fri, Feb 11, 2022, 5:13 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -10,8 +10,8 @@ WHERE person_id = (SELECT id FROM people WHERE name IN ("Johnny Depp", "Helena B
 
 SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
-WHERE person_id = (SELECT id FROM people WHERE name = "Johnny Depp")
-AND (SELECT id FROM people WHERE name = "Helena Bonham Carter");
+WHERE person_id IN ((SELECT id FROM people WHERE name = "Johnny Depp")
+AND (SELECT id FROM people WHERE name = "Helena Bonham Carter"));
 
 SELECT title, person_id FROM movies JOIN stars ON id = movie_id
 WHERE title = "Sweeney Todd: The Demon Barber of Fleet Street";
