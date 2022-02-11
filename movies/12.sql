ebfe96ebfa4b8315e4c379ba63f118a6039da1e1
brucette <89015988+brucette@users.noreply.github.com>
2022-02-11 15:31:33 +0000

    Fri, Feb 11, 2022, 3:31 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -6,3 +6,10 @@ In 12.sql, write a SQL query to list the titles of all movies in which both John
 
 SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
 WHERE person_id = (SELECT id FROM people WHERE name IN ("Johnny Depp", "Helena Bonham Carter"));
+
+SELECT title, person_id FROM movies JOIN stars ON id = movie_id
+WHERE title = "Sweeney Todd: The Demon Barber of Fleet Street";
+
+
+JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
+WHERE person_id = (SELECT id FROM people WHERE name IN ("Johnny Depp", "Helena Bonham Carter"));
