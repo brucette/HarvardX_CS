e12a49c869dc9b822eeee486cddfdf6408302bac
brucette <89015988+brucette@users.noreply.github.com>
2022-02-11 15:38:43 +0000

    Fri, Feb 11, 2022, 3:38 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -14,4 +14,6 @@ SELECT title FROM movies JOIN stars ON id = movie_id
 WHERE person_id IN (SELECT id FROM people WHERE name IN ("Johnny Depp", "Helena Bonham Carter");
 
 JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
+
+
 WHERE person_id = (SELECT id FROM people WHERE name "Johnny Depp") AND WHERE person_id = (SELECT id FROM people WHERE name = "Helena Bonham Carter");
