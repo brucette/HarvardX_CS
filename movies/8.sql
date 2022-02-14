a7872cc317399174196d443f02fec9356a70a831
brucette <89015988+brucette@users.noreply.github.com>
2022-02-14 14:41:56 +0000

    Mon, Feb 14, 2022, 2:41 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(name) FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE movie_id = (SELECT id FROM movies WHERE title = "Toy Story");
+SELECT COUNT(name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE title = "Toy Story";
 
 
 /*In 8.sql, write a SQL query to list the names of all people who starred in Toy Story.
