d85be9c935758d8c0829a0d2950ad19ee298a3d8
brucette <89015988+brucette@users.noreply.github.com>
2022-02-14 15:24:39 +0000

    Mon, Feb 14, 2022, 3:24 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,3 +1,9 @@
-SELECT title FROM movies JOIN stars ON id = stars.movie_id JOIN ratings ON id = ratings.movie_id
-WHERE person_id = (SELECT id FROM people WHERE name = 'Chadwick Boseman')
+SELECT title
+  FROM movies
+       JOIN stars
+         ON id = stars.movie_id
+
+       JOIN ratings
+         ON id = ratings.movie_id
+ WHERE person_id = (SELECT id FROM people WHERE name = 'Chadwick Boseman')
 ORDER BY rating DESC LIMIT 5;
--- movies/9.sql
+++ movies/9.sql
@@ -1,5 +1,9 @@
-/*SELECT COUNT(DISTINCT name) FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id
-WHERE stars.movie_id IN (SELECT id FROM movies where year = 2004) ORDER BY birth;*/
+SELECT DISTINCT name
+  FROM people
+       JOIN stars
+         ON people.id = stars.person_id
 
-SELECT DISTINCT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id
-WHERE year = 2004 ORDER BY birth;
+       JOIN movies
+         ON stars.movie_id = movies.id
+ WHERE year = 2004
+ ORDER BY birth;
