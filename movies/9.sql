6a7c8e20c958d1d6a43bb2f89af07d16aec1e4f1
brucette <89015988+brucette@users.noreply.github.com>
2022-02-14 14:46:33 +0000

    Mon, Feb 14, 2022, 2:46 PM +00:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,2 +1,5 @@
+/*SELECT COUNT(DISTINCT name) FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id
+WHERE stars.movie_id IN (SELECT id FROM movies where year = 2004) ORDER BY birth;*/
+
 SELECT DISTINCT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id
-WHERE stars.movie_id IN (SELECT id FROM movies where year = 2004) ORDER BY birth;
+WHERE year = 2004 ORDER BY birth;
