4c2713886f34cfba97ec9d9be288492182ac8154
brucette <89015988+brucette@users.noreply.github.com>
2022-02-14 14:06:02 +0000

    Mon, Feb 14, 2022, 2:06 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -5,7 +5,5 @@ In 13.sql, write a SQL query to list the names of all people who starred in a mo
     Kevin Bacon himself should not be included in the resulting list.
 
 SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id
-WHERE movies.id
-
-SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
-WHERE name = "Kevin Bacon" AND birth = 1958;
+WHERE title IN (SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
+WHERE name = "Kevin Bacon" AND birth = 1958);
