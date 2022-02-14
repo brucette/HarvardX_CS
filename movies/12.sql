5746f823cb3cc7782b70f4f600b7e1b7b2b5e95e
brucette <89015988+brucette@users.noreply.github.com>
2022-02-14 13:41:35 +0000

    Mon, Feb 14, 2022, 1:41 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,28 +1,3 @@
-In 12.sql, write a SQL query to list the titles of all movies in which both Johnny Depp and Helena Bonham Carter starred.
-    Your query should output a table with a single column for the title of each movie.
-    You may assume that there is only one person in the database with the name Johnny Depp.
-    You may assume that there is only one person in the database with the name Helena Bonham Carter.
-
-
-SELECT title FROM movies
-JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
-WHERE person_id = ALL (SELECT id FROM people WHERE name IN ("Johnny Depp", "Helena Bonham Carter"));
-
-SELECT title FROM movies
-JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
-WHERE person_id IN ((SELECT id FROM people WHERE name = "Johnny Depp")
-AND person_id IN (SELECT id FROM people WHERE name = "Helena Bonham Carter"));
-
-SELECT title FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE name = "Johnny Depp"
+SELECT title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE name = "Johnny Depp"
 INTERSECT
-SELECT title FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE name = "Helena Bonham Carter";
-
-SELECT title FROM movies
-JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
-WHERE EXISTS ((SELECT id FROM people WHERE name = "Johnny Depp") AND (SELECT id FROM people WHERE name = "Helena Bonham Carter"));
+SELECT title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE name = "Helena Bonham Carter";
