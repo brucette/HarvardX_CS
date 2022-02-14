5134c81a823fd1735f8a39e01f979f5ea9c7f480
brucette <89015988+brucette@users.noreply.github.com>
2022-02-14 14:18:11 +0000

    Mon, Feb 14, 2022, 2:18 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -8,5 +8,4 @@ SELECT COUNT(DISTINCT name) FROM people JOIN stars ON people.id = stars.person_i
 WHERE title IN (SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
 WHERE name = "Kevin Bacon" AND birth = 1958)
 EXCEPT
-SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id
-WHERE name = "Kevin Bacon" AND birth = 1958;
+SELECT name FROM people WHERE name = "Kevin Bacon" AND birth = 1958;
