cee1db5bd29938645237d787bc5f0535f90b1269
brucette <89015988+brucette@users.noreply.github.com>
2022-02-14 15:31:34 +0000

    Mon, Feb 14, 2022, 3:31 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,3 +1,19 @@
-SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE name = "Johnny Depp"
+SELECT title
+  FROM movies
+       JOIN stars
+         ON movies.id = stars.movie_id
+
+       JOIN people
+         ON stars.person_id = people.id
+ WHERE name = "Johnny Depp"
+
 INTERSECT
-SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE name = "Helena Bonham Carter";
+
+SELECT title
+  FROM movies
+       JOIN stars
+         ON movies.id = stars.movie_id
+
+       JOIN people
+         ON stars.person_id = people.id
+ WHERE name = "Helena Bonham Carter";
