ccad9e7a9268b26c159fd7e7169362f60dfd054a
brucette <89015988+brucette@users.noreply.github.com>
2022-02-14 14:50:57 +0000

    Mon, Feb 14, 2022, 2:50 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -3,5 +3,5 @@ WHERE person_id = (SELECT id FROM people WHERE name = 'Chadwick Boseman')
 ORDER BY rating DESC LIMIT 5;
 
 SELECT title FROM movies JOIN stars ON id = stars.movie_id JOIN ratings ON id = ratings.movie_id
-WHERE person_id = (SELECT id FROM people WHERE name = 'Chadwick Boseman')
+WHERE name = 'Chadwick Boseman'
 ORDER BY rating DESC LIMIT 5;
