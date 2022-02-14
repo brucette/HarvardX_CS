c71357bd51619a28c8f24ea1b04fc436af6eda96
brucette <89015988+brucette@users.noreply.github.com>
2022-02-14 15:50:27 +0000

    Mon, Feb 14, 2022, 3:50 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,5 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- First query to see what tables are included:
+.tables
+
+-- Second query to take a look at the crime scene report table:
 
 -- List of suspects
