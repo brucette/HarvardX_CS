6423802f74f7c983df795cf422adf6fbb5a5b801
brucette <89015988+brucette@users.noreply.github.com>
2022-02-14 14:49:30 +0000

    Mon, Feb 14, 2022, 2:49 PM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT name FROM people JOIN directors ON people.id = directors.person_id JOIN ratings ON directors.movie_id = ratings.movie_id
-WHERE directors.movie_id IN (SELECT movie_id FROM ratings WHERE rating >= 9.0);
+/*SELECT DISTINCT name FROM people JOIN directors ON people.id = directors.person_id JOIN ratings ON directors.movie_id = ratings.movie_id
+WHERE directors.movie_id IN (SELECT movie_id FROM ratings WHERE rating >= 9.0);*/
 
 SELECT DISTINCT name FROM people JOIN directors ON people.id = directors.person_id JOIN ratings ON directors.movie_id = ratings.movie_id
 WHERE rating >= 9.0;
