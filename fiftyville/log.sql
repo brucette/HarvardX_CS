5ffb6fc42ecd1c39e2c5a9f25d92e055d0345387
brucette <89015988+brucette@users.noreply.github.com>
2022-02-15 14:09:31 +0000

    Tue, Feb 15, 2022, 2:09 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -121,7 +121,7 @@ INTERSECT
 
 INTERSECT
 
-   SELECT name, license_plate
+   SELECT license_plate, name
      FROM bakery_security_logs
           JOIN people
             ON bakery_security_logs.license_plate = people.license_plate
