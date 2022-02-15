49420346c4b7441b112ee28c62f71c1197508e9a
brucette <89015988+brucette@users.noreply.github.com>
2022-02-15 12:22:19 +0000

    Tue, Feb 15, 2022, 12:22 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -89,7 +89,7 @@ flight the following day*/
    WHERE month = 7 AND day = 28 AND year = 2021 AND duration < 60;
 
   -- check who made a call of less than 60 s:
-  SELECT caller.name, receiver.name
+  SELECT people.caller.name
     FROM phone_calls
          JOIN people
            ON phone_calls.receiver = people.phone_number
