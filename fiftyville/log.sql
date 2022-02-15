987b9b5a3edae7c7bc39d77d142c7b97edd15a8c
brucette <89015988+brucette@users.noreply.github.com>
2022-02-15 10:14:20 +0000

    Tue, Feb 15, 2022, 10:14 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -63,7 +63,8 @@ end of the phone to purchase the flight ticket. |
          JOIN atm_transactions
            ON atm_transactions.account_number = bank_accounts.account_number
    WHERE month = 7 AND day = 28 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
-   INTERSECT
-   SELECT
-   
+INTERSECT
+  SELECT name
+    FROM bakery_security_logs
+
 -- List of suspects
