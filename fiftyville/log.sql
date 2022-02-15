b3b9c1d91077c930e381a5b3245082b1ae197189
brucette <89015988+brucette@users.noreply.github.com>
2022-02-15 19:23:38 +0000

    Tue, Feb 15, 2022, 7:23 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -65,7 +65,7 @@ SELECT description
   Iman
   Luca */
 
-/* Check call, flight and passenger logs to see who where on a call around the time the thief left the bakery, as witness Raymond
+/* Check call logs to see who where on a call around the time the thief left the bakery, as witness Raymond
  overheard a call that lasted less than a minute in which the thief planned to leave with the first flight the following day*/
 .schema phone_calls
 
