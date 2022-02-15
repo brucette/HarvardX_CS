ea451f7239a7f582eb7a283d9a305c791e46d9a0
brucette <89015988+brucette@users.noreply.github.com>
2022-02-15 08:26:12 +0000

    Tue, Feb 15, 2022, 8:26 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -51,4 +51,5 @@ end of the phone to purchase the flight ticket. |
   .schema
   SELECT id, account_number, transaction_type
     FROM atm_transactions
+   WHERE month = 7 AND day = 28 AND hour = 8 AND minute BETWEEN 15 AND 30;
 -- List of suspects
