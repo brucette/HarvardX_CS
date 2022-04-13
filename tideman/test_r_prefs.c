8b918e52e02021ec64e6a864f74f4e9a1b8f2e72
brucette <89015988+brucette@users.noreply.github.com>
2022-04-13 09:02:03 +0000

    Wed, Apr 13, 2022, 9:02 AM +00:00

--- tideman/test_r_prefs.c
+++ tideman/test_r_prefs.c
@@ -28,7 +28,7 @@ void add_pairs(void)
     {
         for (int j = 0; j < 3; j++)
         {
-            if (preferences[i][j] > voter_count / 2)
+            if (preferences[i][j] > candidate_count / voter_count)
             {
                 pair.winner = ;
                 pair.loser = ;
