33fca07e5952d72f4ad13efed15341ddcb9541c4
brucette <89015988+brucette@users.noreply.github.com>
2022-04-14 13:50:32 +0000

    Thu, Apr 14, 2022, 1:50 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -213,6 +213,8 @@ void lock_pairs(void)
     // TODO
     int edge = 0;
     int limit = candidate_count -1;
+    int losers[pair_count ];
+    int winners[];
 
     for (int i = 0; i < pair_count - 1; i++)
     {
