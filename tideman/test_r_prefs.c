2d595b6fcf435cf0d53d1eea65451c6eaed428ec
brucette <89015988+brucette@users.noreply.github.com>
2022-04-20 17:14:01 +0000

    Wed, Apr 20, 2022, 5:14 PM +00:00

--- tideman/test_r_prefs.c
+++ tideman/test_r_prefs.c
@@ -122,23 +122,20 @@ Charlie
 Bob
 Alice
 
-
 Dean
 Charlie
 Alice
 Bob
 
-
-
 pair_count = A&B, A&C, A&D, B&C, B&D, C&D
 
 pairs =
-{A - 3      {A - 2      {D - 2      {B - 3      {B - 2
- B - 1} ,    C - 2} ,    A - 2} ,    C - 2} ,    D - 2}
+{A - 4      {A - 2      {D - 3      {B - 2      {B - 2
+ B - 1} ,    C - 3} ,    A - 2} ,    C - 3} ,    D - 2}
 
 winner pairs =
 
 A - 3       B - 2       C - 3
-B - 1  ,    C - 2  ,    D - 1
+B - 1  ,    C - 2  ,    D - 2
 
 
