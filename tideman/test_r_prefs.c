87584edd1bf313331be52e9eca85d5a466dda406
brucette <89015988+brucette@users.noreply.github.com>
2022-04-20 16:11:50 +0000

    Wed, Apr 20, 2022, 4:11 PM +00:00

--- tideman/test_r_prefs.c
+++ tideman/test_r_prefs.c
@@ -131,4 +131,4 @@ pair_count = A&B, A&C, A&D, B&C, B&D, C&D
 
 pairs =
 {A - 4      {A - 3      {D - 3      {B - 3      {B - 2
- B - 1} ,    C - 2} ,    A - 2} ,    C - 1} ,    D - 2}
+ B - 1} ,    C - 2} ,    A - 2} ,    C - 2} ,    D - 2}
