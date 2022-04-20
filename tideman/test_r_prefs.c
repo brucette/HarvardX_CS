148180a2b2b2385f8db5243ba9532f04e4f5540f
brucette <89015988+brucette@users.noreply.github.com>
2022-04-20 16:13:48 +0000

    Wed, Apr 20, 2022, 4:13 PM +00:00

--- tideman/test_r_prefs.c
+++ tideman/test_r_prefs.c
@@ -127,5 +127,5 @@ Alice
 pair_count = A&B, A&C, A&D, B&C, B&D, C&D
 
 pairs =
-{A - 3      {A - 2      {D - 3      {B - 3      {B - 2
+{A - 3      {A - 2      {D - 2      {B - 3      {B - 2
  B - 1} ,    C - 2} ,    A - 2} ,    C - 2} ,    D - 2}
