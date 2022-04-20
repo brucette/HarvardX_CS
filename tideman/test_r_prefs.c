be52a8da86841d666e20db58070dbbb7967c05f9
brucette <89015988+brucette@users.noreply.github.com>
2022-04-20 16:12:49 +0000

    Wed, Apr 20, 2022, 4:12 PM +00:00

--- tideman/test_r_prefs.c
+++ tideman/test_r_prefs.c
@@ -122,13 +122,10 @@ Bob
 Charlie
 Alice
 
-Dean
-Alice
-Charlie
-Bob
+
 
 pair_count = A&B, A&C, A&D, B&C, B&D, C&D
 
 pairs =
-{A - 4      {A - 3      {D - 3      {B - 3      {B - 2
+{A - 3      {A - 2      {D - 3      {B - 3      {B - 2
  B - 1} ,    C - 2} ,    A - 2} ,    C - 2} ,    D - 2}
