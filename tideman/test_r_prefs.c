5c5a8f2ff5bca4bdcb66ff4e7d9440538bd6e03c
brucette <89015988+brucette@users.noreply.github.com>
2022-04-20 16:09:35 +0000

    Wed, Apr 20, 2022, 4:09 PM +00:00

--- tideman/test_r_prefs.c
+++ tideman/test_r_prefs.c
@@ -130,5 +130,5 @@ Bob
 pair_count = A&B, A&C, A&D, B&C, B&D, C&D
 
 pairs =
-{A - 5      {A - 3      {D - 3      {B - 3      {B - 2
+{A - 4      {A - 3      {D - 3      {B - 3      {B - 2
  B - 1} ,    C - 2} ,    A - 2} ,    C - 1} ,    D - 2}
