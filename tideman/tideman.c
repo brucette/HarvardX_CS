1f7378d9ec3c3829d1951e848015f1b4075f57d3
brucette <89015988+brucette@users.noreply.github.com>
2022-04-21 09:52:21 +0000

    Thu, Apr 21, 2022, 9:52 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -289,7 +289,7 @@ void print_winner(void)
         //printf("\n");
     }
     //printf("%i", winner);
-    for (int k = 0; k < pair_count; k++)
+    for (int k = 0; k < ; k++)
     {
         printf("%s", candidates[winners[k]]);
         printf("\n");
