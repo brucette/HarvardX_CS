ca78413b54f8ff7cbc118b25ba1668c066de53e3
brucette <89015988+brucette@users.noreply.github.com>
2022-04-21 10:32:15 +0000

    Thu, Apr 21, 2022, 10:32 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -281,7 +281,7 @@ void print_winner(void)
             {
                 wins++;
             }
-            if (wins == pair_count)
+            if (wins == candidate_count - 1)
                 {
                     winners[index] = i;
                     index++;
