994bba16f5cf429ef9c561e64549728598085aca
brucette <89015988+brucette@users.noreply.github.com>
2022-04-29 13:31:03 +0000

    Fri, Apr 29, 2022, 1:31 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -147,7 +147,6 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
     for (int i = 0; i < candidate_count; i++)
     {
         //compare candidate voter gave to all the candidates running
