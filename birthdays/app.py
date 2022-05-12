725496d912ca06343875866ecb7954c1cffe1489
brucette <89015988+brucette@users.noreply.github.com>
2022-05-12 15:26:50 +0000

    Thu, May 12, 2022, 3:26 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -33,7 +33,7 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-        db.execute("SELECT name, month, day")
+        db.execute("SELECT FROM birthdays name, month, day")
         return render_template("index.html")
 
 
