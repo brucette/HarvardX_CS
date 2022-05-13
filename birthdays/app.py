158b14f41320fce3f7cf4ff0c23ed469e70702ac
brucette <89015988+brucette@users.noreply.github.com>
2022-05-13 19:05:56 +0000

    Fri, May 13, 2022, 7:05 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -36,7 +36,6 @@ def index():
 
         # TODO: Display the entries in the database on index.html
         entries = db.execute("SELECT * FROM birthdays")
-
         return render_template("index.html", entries=entries)
 
 
