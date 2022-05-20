c729e793b03d1beb17717192e0a1d4e472118582
brucette <89015988+brucette@users.noreply.github.com>
2022-05-20 14:06:18 +0000

    Fri, May 20, 2022, 2:06 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -66,7 +66,8 @@ def buy():
         # Check if user has enough cash
         details = lookup(stock)
         price = details["price"] * shares
-        funds = db.execute("SELECT cash FROM users WHERE id = ?", session.get("user_id"))
+        funds = 10000
+        #db.execute("SELECT cash FROM users WHERE id = ?", session.get("user_id"))
         if funds - price < 0:
             return apology("funds exceeded")
 
