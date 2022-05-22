ee7fc82522661d5b7fd8084732f2eb9bdb823bc6
brucette <89015988+brucette@users.noreply.github.com>
2022-05-22 18:53:57 +0000

    Sun, May 22, 2022, 6:53 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -46,7 +46,7 @@ def after_request(response):
 @login_required
 def index():                                    #4
     """Show portfolio of stocks"""
-     #db.execute("SELECT FROM purchases  )
+     #stocks = db.execute("SELECT stock FROM purchases WHERE   )
     return render_template("index.html")
     #return apology("INDEX HERE")
 
