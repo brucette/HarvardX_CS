e39543a896cef8dce79899d4fdebe95f64954288
brucette <89015988+brucette@users.noreply.github.com>
2022-05-24 11:18:58 +0000

    Tue, May 24, 2022, 11:18 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -47,11 +47,12 @@ def after_request(response):
 def index():                                    #4
     """Show portfolio of stocks"""
     #portfolio = db.execute("SELECT * FROM purchases WHERE user_id = ?", session["user_id"])
-    portfolio = db.execute("SELECT stock, SUM(shares) FROM purchases WHERE user_id = ? GROUP BY stock", session["user_id"])
+    stock_shares = db.execute("SELECT stock, SUM(shares) FROM purchases WHERE user_id = ? GROUP BY stock", session["user_id"])
+    cprice_totalv = 
     all_stocks = db.execute("SELECT stock FROM purchases WHERE user_id = ?", session["user_id"])
     current_prices = []
 
-    for stock in portfolio:
+    for stock in stock_shares:
         print(stock)
         current_price = lookup(stock["stock"])
         current_prices.append(current_price["price"])
