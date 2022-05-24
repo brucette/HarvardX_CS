81c58067925f13f8a4885087ff6ea30915771f31
brucette <89015988+brucette@users.noreply.github.com>
2022-05-24 10:36:22 +0000

    Tue, May 24, 2022, 10:36 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -46,7 +46,8 @@ def after_request(response):
 @login_required
 def index():                                    #4
     """Show portfolio of stocks"""
-    portfolio = db.execute("SELECT * FROM purchases WHERE user_id = ?", session["user_id"])
+    #portfolio = db.execute("SELECT * FROM purchases WHERE user_id = ?", session["user_id"])
+    portfolio = db.execute("SELECT stock, SUM(shares), FROM purchases WHERE user_id = ? GROUP BY stock", session["user_id"])
     all_stocks = db.execute("SELECT stock FROM purchases WHERE user_id = ?", session["user_id"])
     current_prices = []
 
