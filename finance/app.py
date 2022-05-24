5aacc515f811182c5af107333da052c92bd9e2c9
brucette <89015988+brucette@users.noreply.github.com>
2022-05-24 13:10:12 +0000

    Tue, May 24, 2022, 1:10 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -47,20 +47,16 @@ def after_request(response):
 def index():                                    #4
     """Show portfolio of stocks"""
     #portfolio = db.execute("SELECT * FROM purchases WHERE user_id = ?", session["user_id"])
+    #all_stocks = db.execute("SELECT stock FROM purchases WHERE user_id = ?", session["user_id"])
     portfolio = db.execute("SELECT stock, SUM(shares) FROM purchases WHERE user_id = ? GROUP BY stock", session["user_id"])
-    all_stocks = db.execute("SELECT stock FROM purchases WHERE user_id = ?", session["user_id"])
     current_prices = {}
 
-    for stock in portfolio:
+    for item in portfolio:
         print(stock)
-        current_price = lookup(stock["stock"])
-        current_prices[current_price["symbol"]] = current_price["price"] #["price"]
-
-    for item in current_prices:
-        print("current price:", item)
+        current_price = lookup(item["stock"])
+        current_prices[current_price["symbol"]] = current_price["price"]
 
     return render_template("index.html", portfolio=portfolio, current_prices=current_prices)
-    #return apology("INDEX HERE")
 
 
 @app.route("/buy", methods=["GET", "POST"])     #3
