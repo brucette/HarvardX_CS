b205341bcd37605c8834593f5c137878b25eb262
brucette <89015988+brucette@users.noreply.github.com>
2022-05-30 16:27:51 +0000

    Mon, May 30, 2022, 4:27 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -50,6 +50,9 @@ def index():                                    #4
         #portfolio = db.execute("SELECT * FROM purchases WHERE user_id = ?", session["user_id"])
         #all_stocks = db.execute("SELECT stock FROM purchases WHERE user_id = ?", session["user_id"])
         portfolio = db.execute("SELECT type, stock, SUM(shares) FROM transactions WHERE user_id = ? AND type = ? GROUP BY stock", session["user_id"], "purchase")
+
+        if
+
         current_prices = {}
         funds = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
         total_value = 0
