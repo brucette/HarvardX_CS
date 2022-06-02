d1e9497b68ef43aad75f5db2e2b2e12a1efaa650
brucette <89015988+brucette@users.noreply.github.com>
2022-06-02 13:14:09 +0000

    Thu, Jun 2, 2022, 1:14 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -298,7 +298,6 @@ def sell():
         shares_entered = int(request.form.get("shares"))
         # **********************************UPDATE THE FOLLOWING:**********************************************************************************************************
         #portf_shares = db.execute("SELECT SUM(shares) FROM transactions WHERE user_id = ? AND stock = ? AND type = ? GROUP BY stock", session["user_id"], stock, "purchase")
-        actual_shares = portf_shares[0]["SUM(shares)"]
 
         if not shares_entered or shares_entered <= 0 or shares_entered > actual_shares[stock]:
             return apology("invalid number of shares")
