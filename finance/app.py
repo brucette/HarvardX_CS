1d3a049ada8fcb3a7f832149392c70fa5f73b9c6
brucette <89015988+brucette@users.noreply.github.com>
2022-06-02 10:09:55 +0000

    Thu, Jun 2, 2022, 10:09 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -94,7 +94,8 @@ def index():                                    #4
                 print(all_bought[0])
                 print(all_sold)
                 #print(difference)
-
+                print(len(all_bought))
+                print(len(all_sold))
         return render_template("index.html", owned_stocks=owned_stocks, current_prices=current_prices, funds=funds, total_value=total_value)
 
 @app.route("/buy", methods=["GET", "POST"])     #3
