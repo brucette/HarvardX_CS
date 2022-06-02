8e391d804db7eab35579771635d1f6dd7740bae8
brucette <89015988+brucette@users.noreply.github.com>
2022-06-02 13:37:03 +0000

    Thu, Jun 2, 2022, 1:37 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -101,8 +101,8 @@ def buy():
             return apology("must enter valid stock symbol")
 
         # Ensure valid number of shares entered
-        shares = int(request.form.get("shares"))
-        if not shares or shares <= 0:
+        shares = request.form.get("shares"))
+        if not shares or int(shares) <= 0:
             return apology("number of shares missing")
 
         # Check if user has enough cash
