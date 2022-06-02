ff16d1a6ceed3ad2805ec6827b2939b8b6fc7b18
brucette <89015988+brucette@users.noreply.github.com>
2022-06-02 08:42:34 +0000

    Thu, Jun 2, 2022, 8:42 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -90,7 +90,7 @@ def index():                                    #4
                 owned_stocks.append({"stock" : current_price["symbol"], "price" : current_price["price"], "shares" : item["SUM(shares)"]})
             #else:
 
-        return render_template("index.html", bought=bought, current_prices=current_prices, funds=funds, total_value=total_value)
+        return render_template("index.html", owned_stocks=owned_stocks, current_prices=current_prices, funds=funds, total_value=total_value)
 
 @app.route("/buy", methods=["GET", "POST"])     #3
 @login_required
