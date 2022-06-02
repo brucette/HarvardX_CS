cb7f3d42c81f4656ff3d1828383f6e636ef155ac
brucette <89015988+brucette@users.noreply.github.com>
2022-06-02 09:06:59 +0000

    Thu, Jun 2, 2022, 9:06 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -90,6 +90,7 @@ def index():                                    #4
             #else:
                 #calculate if any stocks still available
                 all_bought =
+                all_sold = 
 
 
         return render_template("index.html", owned_stocks=owned_stocks, current_prices=current_prices, funds=funds, total_value=total_value)
