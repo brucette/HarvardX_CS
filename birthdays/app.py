14b8955515aa01abac22c1aa28feb4acad88807a
brucette <89015988+brucette@users.noreply.github.com>
2022-06-09 13:27:20 +0000

    Thu, Jun 9, 2022, 1:27 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -59,7 +59,7 @@ def remove():
 def edit():
     id = request.form.get("id")
     if id:
-        return render_template("edit.html", name=)
+        return render_template("edit.html", edit_name=edit_name)
 
     edit_name = request.form.get("name")
     edit_month = request.form.get("month")
