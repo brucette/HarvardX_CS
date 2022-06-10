b8b3026fb69cd7c7904841e62e18132b9062d3e0
brucette <89015988+brucette@users.noreply.github.com>
2022-06-10 13:54:20 +0000

    Fri, Jun 10, 2022, 1:54 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -48,13 +48,15 @@ def index():
     """Show portfolio of stocks"""
     try:
         # Get bought stock
-        bought = db.execute("SELECT type, stock, SUM(shares) FROM transactions WHERE user_id = ? AND type = ? GROUP BY stock", session["user_id"], "purchase")
+        bought = db.execute(
+            "SELECT type, stock, SUM(shares) FROM transactions WHERE user_id = ? AND type = ? GROUP BY stock", session["user_id"], "purchase")
 
     except RuntimeError:
         return apology("You currently have no stocks to display")
     else:
         # Get sold stock
-        sold = db.execute("SELECT type, stock, SUM(shares) FROM transactions WHERE user_id = ? AND type = ? GROUP BY stock", session["user_id"], "sale")
+        sold = db.execute(
+            "SELECT type, stock, SUM(shares) FROM transactions WHERE user_id = ? AND type = ? GROUP BY stock", session["user_id"], "sale")
         sold_stocks = []
         for item in sold:
             sold_stocks.append(item["stock"])
